5bf0083eeb48b0b5028856833df26ef336225594
RWij86_2 <ricardo@efficienttransition.com>
2026-01-14 22:41:24 +0100

    change of tab.svelte

--- src/lib/Tab.svelte
+++ src/lib/Tab.svelte
@@ -1,57 +1,35 @@
 <script>
-    import { run } from 'svelte/legacy';
+  import { getContext, createEventDispatcher } from "svelte";
 
-    import { getContext , createEventDispatcher, onMount } from "svelte"
-    const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher();
+  const tabStore = getContext("tabStore");
 
-    const tabStore = getContext ("tabStore")
+  let container;
+  let { id, title, emphasized, isSelected } = $props();
 
-    let container = $state()
-    let {
-        id,
-        title,
-        emphasized,
-        isSelected
-    } = $props();
+  function handleClick() {
+    tabStore.id = id;
+    tabStore.boundingBox = container?.getBoundingClientRect();
+    dispatch("tabSelect", { tabID: id, tabName: title });
+  }
 
-
-    function handleClick () {
-        $tabStore.id = id
-        $tabStore.boundingBox = container?.getBoundingClientRect();
-        dispatch("tabSelect", {"tabID": id, "tabName": title } )
+  function syncBoundingBox() {
+    if (isSelected && container) {
+      tabStore.boundingBox = container.getBoundingClientRect();
     }
-    function init() {
-      if (isSelected)
-        $tabStore.boundingBox = container?.getBoundingClientRect();
-    }
+  }
 
-    onMount( () => init() )
-
-    run(() => {
-        init (id, title, emphasized, $tabStore.id, isSelected)
-    });
+  // rerun wanneer isSelected / container verandert
+  $effect(syncBoundingBox);
 </script>
 
-<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
-<!-- svelte-ignore a11y_click_events_have_key_events -->
-<div 
-    bind:this={container}
-    class="spectrum-Tabs-item"
-    class:is-selected={isSelected}
-    class:emphasized={isSelected && emphasized}
-    tabindex="0"
-    onclick={handleClick}>
-        <span class="spectrum-Tabs-itemLabel"> {title} </span>
+<div
+  bind:this={container}
+  class="spectrum-Tabs-item"
+  class:is-selected={isSelected}
+  class:emphasized={isSelected && emphasized}
+  tabindex="0"
+  onclick={handleClick}
+>
+  <span class="spectrum-Tabs-itemLabel">{title}</span>
 </div>
-
-<style>
-    .emphasized {
-      color: var(--emphasizedColor) !important;
-    }
-    .spectrum-Tabs-item {
-      color: var(--spectrum-global-color-gray-600);
-    }
-    .spectrum-Tabs-item.is-selected {
-      color: var(--spectrum-global-color-gray-900);
-    }
-</style>
